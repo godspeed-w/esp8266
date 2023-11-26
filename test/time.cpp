b564c73c4fe8a8be7f4916856f79ece7238e64aa
godspeed-w <2399573003@.qq.com>
2023-11-27 00:31:48 +0800

    增加测试

---
+++ test/time.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+
+int main()
+{
+    time_t timep = 1701012395;
+    struct tm *p;
+    char time1[28];
+    // time(&timep);
+    p = gmtime(&timep);
+    printf("%d\t", p->tm_sec); /*获取当前秒*/
+    printf("%d\t", p->tm_min); /*获取当前分*/
+    printf("%d\t", 8 + p->tm_hour); /*获取当前时,这里获取西方的时间,刚好相差八个小时*/
+    printf("%d\t", p->tm_mday); /*获取当前月份日数,范围是1-31*/
+    printf("%d\t", 1 + p->tm_mon); /*获取当前月份,范围是0-11,所以要加1*/
+    printf("%d\t", 1900 + p->tm_year); /*获取当前年份,从1900开始，所以要加1900*/
+    printf("%d\n", p->tm_yday); /*从今年1月1日算起至今的天数，范围为0-365*/
+    // 将它们合成一个字符串
+    sprintf(time1, "%d年%d月%d日 %d:%d:%d", 1900 + p->tm_year, 1 + p->tm_mon, p->tm_mday, 8 + p->tm_hour, p->tm_min, p->tm_sec);
+    printf("当前时间为：%s\n", time1);
+}
